8ef9952e9366c6b2ec22d78cd08196bb4aff316c
András Csányi <andras.csanyi@pm.me>
2022-02-20 11:29:50 +0100

    Dockerize and move to java (#7)

---
+++ src/test/java/org/encyclopediagalactica/sourceformat/webapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.encyclopediagalactica.sourceformat.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
